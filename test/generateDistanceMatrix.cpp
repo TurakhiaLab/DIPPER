a32ccb231e96b8f8b4521571cade99d41737a4b6
zec022 <zec022@AD.UCSD.EDU@peregrine.ucsd.edu>
2024-03-20 16:24:45 -0700

    update script to generated distance matrix

---
+++ test/generateDistanceMatrix.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    mt19937 myrnd(time(NULL));
+    int n;
+    cin>>n;
+    double **dis = new double*[n];
+    for (int i = 0; i < n; ++i) {
+        dis[i] = new double[n];
+    }
+    for(int i=0;i<n;i++)
+        for(int j=i+1;j<n;j++)
+            dis[i][j]=dis[j][i]=double(myrnd()%100000)/100000;
+    for(int i=0;i<n;i++){
+        printf("Node_%d\t",i);
+        for(int j=0;j<n;j++){
+            printf("%.6lf%c",dis[i][j],j+1==n?'\n':'\t');
+        }
+    }
+    return 0;
+}
