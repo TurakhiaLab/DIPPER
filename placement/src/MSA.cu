f36d5c5436577a0e5213c816df23a2279ce9b404
zec022 <139452560+zec022@users.noreply.github.com>
2024-11-05 07:40:45 -0800

    Update MSA.cu

--- placement/src/MSA.cu
+++ placement/src/MSA.cu
@@ -82,7 +82,8 @@ void MashPlacement::MSADeviceArrays::deallocateDeviceArrays(){
 #define DIST_JUKESCANTOR 2
 #define DIST_TAJIMANEI 3
 #define DIST_KIMURA2P 4
-#define DIST_KIMURA 5
+#define DIST_TAMURA 5
+#define DIST_JINNEI 6
 
 
 __device__ void calculateParams(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int & useful, int & match){
@@ -94,6 +95,62 @@ __device__ void calculateParams(int tarRowId, int curRowId, int seqLen, uint64_t
             int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
             if(et<4||ec<4) useful++;
             if(et<4&&et==ec) match++;
+        }
+    }
+}
+
+__device__ void calculateParams_TJ(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int * frac, int &tot, int &match, int * pr){
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+    for(int i=0;i<compLen;i++){
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            frac[ec]++, frac[et]++, tot++;
+            if(ec>et){
+                int temp=ec;
+                ec=et,et=temp;
+            }
+            if(ec==et) match++;
+            if(ec==0&&et==2) pr[0]++;
+            else if(ec==0&&et==3) pr[1]++;
+            else if(ec==1&&et==2) pr[2]++;
+            else if(ec==1&&et==3) pr[3]++;
+        }
+    }
+}
+
+__device__ void calculateParams_K2P(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int &p, int &q, int &tot){
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+    for(int i=0;i<compLen;i++){
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            tot++;
+            if(et==ec) continue;
+            if(et%2==ec%2) p++;
+            else q++;
+        }
+    }
+}
+
+__device__ void calculateParams_TAMURA(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int &p, int &q, int &tot, int &gc1, int &gc2){
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+    for(int i=0;i<compLen;i++){
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            tot++;
+            if(et==ec) continue;
+            if(et%2==ec%2) p++;
+            else q++;
+            if(ec==1||ec==2) gc1++;
+            if(et==1||et==2) gc2++;
         }
     }
 }
@@ -116,6 +173,33 @@ __global__ void MSADistConstruction(
         if(distanceType==DIST_UNCORRECTED) dist[idx]=uncor;
         else dist[idx]=-0.75*log(1.0-uncor/0.75);
         // printf("%d %d %d %d\n",rowId, idx, match, useful);
+    }
+    else if(distanceType==DIST_TAJIMANEI){
+        int frac[4]={},pr[4]={},tot=0,match=0;
+        double fr[4]={};
+        calculateParams_TJ(rowId, idx, seqLen, compressedSeqs, frac, tot, match, pr);
+        for(int i=0;i<4;i++) fr[i]=double(frac[i])/tot/2.0;
+        double h=0;
+        h+=0.5*pr[0]*fr[0]*fr[2];
+        h+=0.5*pr[1]*fr[0]*fr[3];
+        h+=0.5*pr[2]*fr[1]*fr[2];
+        h+=0.5*pr[3]*fr[1]*fr[3];
+        double D=double(tot-match)/tot;
+        double b=0.5*(1.0-fr[0]*fr[0]-fr[2]*fr[2]+D*D/h);
+        dist[idx]=-b*log(1.0-D/b);
+    }
+    else if(distanceType==DIST_KIMURA2P||distanceType==DIST_JINNEI){
+        int p=0,q=0,tot=0;
+        calculateParams_K2P(rowId, idx, seqLen, compressedSeqs, p, q, tot);
+        double pp=double(p)/tot,qq=double(q)/tot;
+        if(distanceType==DIST_KIMURA2P) dist[idx]=-0.5*log((1-2*pp-qq)*sqrt(1-2*qq));
+        else dist[idx]=0.5*(1.0/(1-2*pp-qq)+0.5/(1-qq*2)-1.5);
+    }
+    else if(distanceType==DIST_TAMURA){
+        int p=0,q=0,tot=0,gc1=0,gc2=0;
+        calculateParams_TAMURA(rowId, idx, seqLen, compressedSeqs, p, q, tot, gc1, gc2);
+        double pp=double(p)/tot,qq=double(q)/tot, c=double(gc1)/tot+double(gc2)/tot-2*double(gc1)*double(gc2)/tot/tot;
+        dist[idx]=-c*log(1-pp/c-qq)-0.5*(1-c)*log(1-2*qq);
     }
     else dist[idx]=0.0;
 }
