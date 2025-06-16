b25d08123cea4a79748f5a96eadb735427548ee1
swalia <swalia@AD.UCSD.EDU@peregrine.ucsd.edu>
2025-06-16 13:55:51 -0700

    added install through cmake

--- install/installUbuntu.sh
+++ install/installUbuntu.sh
@@ -2,11 +2,12 @@
 
 SCRIPT_DIR=$(pwd)
 BUILD_DIR="${SCRIPT_DIR}/../build"
-BIN_DIR="${SCRIPT_DIR}/../bin"
+BIN_DIR="${SCRIPT_DIR}/../"
 
 mkdir -p ${BUILD_DIR}
 cd "${BUILD_DIR}" || exit 1
 cmake ..
 cmake --build . --config Release --parallel
 cmake --install . --prefix ${BIN_DIR}
-export PATH=${BIN_DIR}:$PATH
+export PATH=${BIN_DIR}/bin:$PATH
+cd ${BIN_DIR}
