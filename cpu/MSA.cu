a6976edf677bc7f5b6994e4b19369e3a4dffab1b
y3tseng <y3tseng@AD.UCSD.EDU@peregrine.ucsd.edu>
2025-10-27 12:41:57 -0700

    add MSA cpu version

--- cpu/MSA.cu
+++ cpu/MSA.cu
@@ -128,6 +128,7 @@ void calculateParamsParallel(int tarRowId, int curRowId, int seqLen, uint64_t * 
     }
 }
 
+
 void calculateParams_TJ(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int * frac, int &tot, int &match, int * pr){
     int compLen=(seqLen+15)/16;
     long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
@@ -137,6 +138,37 @@ void calculateParams_TJ(int tarRowId, int curRowId, int seqLen, uint64_t * compr
             int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
             if(et>=4||ec>=4) continue;
             frac[ec]++, frac[et]++, tot++;
+            if(ec>et){
+                int temp=ec;
+                ec=et,et=temp;
+            }
+            if(ec==et) match++;
+            if(ec==0&&et==2) pr[0]++;
+            else if(ec==0&&et==3) pr[1]++;
+            else if(ec==1&&et==2) pr[2]++;
+            else if(ec==1&&et==3) pr[3]++;
+        }
+    }
+}
+
+void calculateParamsParallel_TJ(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int * frac, int &tot, int &match, int * pr){
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+
+    for (int i=0; i<compLen; i++) {
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            if (ec == 0) frac[0]++;
+            else if (ec == 1) frac[1]++;
+            else if (ec == 2) frac[2]++;
+            else if (ec == 3) frac[3]++;
+            if (et == 0) frac[0]++;
+            else if (et == 1) frac[1]++;
+            else if (et == 2) frac[2]++;
+            else if (et == 3) frac[3]++;
+            tot++;
             if(ec>et){
                 int temp=ec;
                 ec=et,et=temp;
@@ -166,10 +198,45 @@ void calculateParams_K2P(int tarRowId, int curRowId, int seqLen, uint64_t * comp
     }
 }
 
+void calculateParamsParallel_K2P(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int &p, int &q, int &tot){
+    int tx=threadIdx.x, bs=blockDim.x, bx=blockIdx.x;
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+    for (int i=0; i<compLen; i++) {
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            tot++;
+            if(et==ec) continue;
+            if(et%2==ec%2) p++;
+            else q++;
+        }
+    }
+}
+
 void calculateParams_TAMURA(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int &p, int &q, int &tot, int &gc1, int &gc2){
     int compLen=(seqLen+15)/16;
     long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
     for(int i=0;i<compLen;i++){
+        long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
+        for(int j=0;j<16&&i*16+j<seqLen;j++){
+            int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
+            if(et>=4||ec>=4) continue;
+            tot++;
+            if(et==ec) continue;
+            if(et%2==ec%2) p++;
+            else q++;
+            if(ec==1||ec==2) gc1++;
+            if(et==1||et==2) gc2++;
+        }
+    }
+}
+
+void calculateParamsParallel_TAMURA(int tarRowId, int curRowId, int seqLen, uint64_t * compressedSeqs, int &p, int &q, int &tot, int &gc1, int &gc2){
+    int compLen=(seqLen+15)/16;
+    long long px=1ll*curRowId*compLen, py=1ll*tarRowId*compLen;
+    for (int i=0; i<compLen; i++) {
         long long vt=compressedSeqs[px+i], vc=compressedSeqs[py+i];
         for(int j=0;j<16&&i*16+j<seqLen;j++){
             int et=(vt>>(j*4))&15, ec=(vc>>(j*4))&15;
@@ -206,11 +273,17 @@ void MSADistConstruction(
         });
     }
     else if(distanceType==DIST_TAJIMANEI){
-        tbb::parallel_for(tbb::blocked_range<int>(0, rowId * threadNum), [&](const tbb::blocked_range<int>& range) {
-        for (int idx = range.begin(); idx < range.end(); ++idx) {
+        tbb::parallel_for(tbb::blocked_range<int>(0, rowId), [&](const tbb::blocked_range<int>& range) {
+        for (int blockID = range.begin(); blockID < range.end(); ++blockID) {
             int frac[4]={},pr[4]={},tot=0,match=0;
             double fr[4]={};
-            calculateParams_TJ(rowId, idx, seqLen, compressedSeqs, frac, tot, match, pr);
+            calculateParamsParallel_TJ(rowId, blockID, seqLen, compressedSeqs, frac, tot, match, pr);
+            // calculateParams_TJ(rowId, idx, seqLen, compressedSeqs, frac, tot, match, pr);
+            if (blockID == 0) {
+                std::cout << "tot: " << tot << " match: " << match << "\n";
+                std::cout << "pr: " << pr[0] << " " << pr[1] << " " << pr[2] << " " << pr[3] << "\n";
+                std::cout << "frac: " << frac[0] << " " << frac[1] << " " << frac[2] << " " << frac[3] << "\n";
+            }
             for(int i=0;i<4;i++) fr[i]=double(frac[i])/tot/2.0;
             double h=0;
             h+=0.5*pr[0]*fr[0]*fr[2];
@@ -219,39 +292,45 @@ void MSADistConstruction(
             h+=0.5*pr[3]*fr[1]*fr[3];
             double D=double(tot-match)/tot;
             double b=0.5*(1.0-fr[0]*fr[0]-fr[2]*fr[2]+D*D/h);
-            dist[idx]=-b*log(1.0-D/b);
+            dist[blockID]=-b*log(1.0-D/b);
+            if (blockID == 0) std::cout << "dist[" << blockID << "] = " << dist[blockID] << "\n";
         }
         });
     }
     else if(distanceType==DIST_KIMURA2P||distanceType==DIST_JINNEI){
-        tbb::parallel_for(tbb::blocked_range<int>(0, rowId * threadNum), [&](const tbb::blocked_range<int>& range) {
-        for (int idx = range.begin(); idx < range.end(); ++idx) {
+        tbb::parallel_for(tbb::blocked_range<int>(0, rowId), [&](const tbb::blocked_range<int>& range) {
+        for (int blockID = range.begin(); blockID < range.end(); ++blockID) {
             int p=0,q=0,tot=0;
-            calculateParams_K2P(rowId, idx, seqLen, compressedSeqs, p, q, tot);
+            // calculateParams_K2P(rowId, idx, seqLen, compressedSeqs, p, q, tot);
+            calculateParamsParallel_K2P(rowId, blockID, seqLen, compressedSeqs, p, q, tot);
             double pp=double(p)/tot,qq=double(q)/tot;
-            if(distanceType==DIST_KIMURA2P) dist[idx]=-0.5*log((1-2*pp-qq)*sqrt(1-2*qq));
-            else dist[idx]=0.5*(1.0/(1-2*pp-qq)+0.5/(1-qq*2)-1.5);
+            if(distanceType==DIST_KIMURA2P) dist[blockID]=-0.5*log((1-2*pp-qq)*sqrt(1-2*qq));
+            else dist[blockID]=0.5*(1.0/(1-2*pp-qq)+0.5/(1-qq*2)-1.5);
         }
         });
     }
     else if(distanceType==DIST_TAMURA){
-        tbb::parallel_for(tbb::blocked_range<int>(0, rowId * threadNum), [&](const tbb::blocked_range<int>& range) {
-        for (int idx = range.begin(); idx < range.end(); ++idx) {
+        tbb::parallel_for(tbb::blocked_range<int>(0, rowId), [&](const tbb::blocked_range<int>& range) {
+        for (int blockID = range.begin(); blockID < range.end(); ++blockID) {
             int p=0,q=0,tot=0,gc1=0,gc2=0;
-            calculateParams_TAMURA(rowId, idx, seqLen, compressedSeqs, p, q, tot, gc1, gc2);
+            calculateParamsParallel_TAMURA(rowId, blockID, seqLen, compressedSeqs, p, q, tot, gc1, gc2);
             double pp=double(p)/tot,qq=double(q)/tot, c=double(gc1)/tot+double(gc2)/tot-2*double(gc1)*double(gc2)/tot/tot;
-            dist[idx]=-c*log(1-pp/c-qq)-0.5*(1-c)*log(1-2*qq);
+            dist[blockID]=-c*log(1-pp/c-qq)-0.5*(1-c)*log(1-2*qq);
         }
         });
     }
     else {
-        for (int idx = 0; idx < rowId * threadNum; ++idx) dist[idx]=0.0;
+        tbb::parallel_for(tbb::blocked_range<int>(0, rowId), [&](const tbb::blocked_range<int>& range) {
+        for (int blockID = range.begin(); blockID < range.end(); ++blockID) {
+            dist[blockID] = 0.0;
+        }
+        });
     }
 }
 
 
 void MashPlacement::MSADeviceArrays::distConstructionOnGpu(Param& params, int rowId, double* d_mashDist) const{
-    int threadNum = 1024, blockNum = 1024; // dont change threadNUM, interally it is used to calculate the distance
+    // int threadNum = 1024, blockNum = 1024; // dont change threadNUM, interally it is used to calculate the distance
     // printf("rowId: %d params.distanceType %d \n", rowId, params.distanceType);
     MSADistConstruction (
         rowId, 
